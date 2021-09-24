15cef41b95ad14cca2d94dee214900957d3134a6
sunjun <sunjun@leinao.ai>
2021-09-24 17:09:19 +0800

    support hierachical classification

---
+++ mmcls/datasets/hierarchical_dataset.py
@@ -0,0 +1,271 @@
+import warnings
+from collections import OrderedDict
+
+import numpy as np
+
+from mmcls.core import average_performance, mAP
+from mmcls.core.evaluation import precision_recall_f1, support
+from mmcls.models.losses import accuracy
+
+from .builder import DATASETS
+from .base_dataset import BaseDataset
+
+
+@DATASETS.register_module()
+class HierarchicalDataset(BaseDataset):
+
+    def __init__(self,
+                 file_type=[
+                     dict(type='ce', max_len=3),
+                     dict(type='bce', max_len=3),
+                 ],
+                 **kwargs,
+                 ):
+        assert file_type is not None
+        assert isinstance(file_type, (list, tuple))
+        assert isinstance(kwargs.get('ann_file', None), (list, tuple))
+        assert len(file_type) == len(kwargs.get('ann_file', []))
+        self.file_type = file_type
+        super(HierarchicalDataset, self).__init__(**kwargs)
+
+    def _combine(self, name_labels):
+        base = name_labels[0]
+        for idx in range(1, len(self.file_type)):
+            assert len(base) == len(name_labels[idx])
+        name_label = dict()
+        for key, value in base.items():
+            for idx in range(1, len(self.file_type)):
+                cvalue = name_labels[idx][key]
+                if not isinstance(value, list):
+                    value = [value]
+                if isinstance(cvalue, list):
+                    value.extend(cvalue)
+                else:
+                    value.append(cvalue)
+            name_label[key] = value
+        return name_label
+
+    def _collect_name_with_label(self):
+        name_labels = dict()
+        for idx in range(len(self.file_type)):
+            name_labels[idx] = dict()
+            file_type = self.file_type[idx]
+            with open(self.ann_file[idx]) as f:
+                for line in f.readlines():
+                    line = line.strip().split(' ')
+                    filename = line[0]
+                    if file_type['type'] == 'ce':
+                        label = line[-1]
+                    elif file_type['type'] == 'bce':
+                        max_len = file_type['max_len']
+                        pos_inds = list(map(int, line[1:]))
+                        label = np.zeros(max_len)
+                        label[pos_inds] = 1
+                        label = label.tolist()
+                    else:
+                        raise ValueError(f"Only support 'ce' and 'bce' file_type")
+                    assert filename not in name_labels[idx]
+                    name_labels[idx][filename] = label
+        name_label = self._combine(name_labels)
+        return name_label
+
+    def load_annotations(self):
+        name_label = self._collect_name_with_label()
+        data_infos = []
+        for filename, gt_label in name_label.items():
+            info = {'img_prefix': self.data_prefix}
+            info['img_info'] = {'filename': filename}
+            info['gt_label'] = np.array(gt_label, dtype=np.int64)
+            data_infos.append(info)
+        return data_infos
+
+    def evaluate_ce(self,
+                    results,
+                    gt_labels,
+                    metric='accuracy',
+                    metric_options=None,
+                    logger=None):
+        """Evaluate the dataset.
+
+        Args:
+            results (list): Testing results of the dataset.
+            metric (str | list[str]): Metrics to be evaluated.
+                Default value is `accuracy`.
+            metric_options (dict, optional): Options for calculating metrics.
+                Allowed keys are 'topk', 'thrs' and 'average_mode'.
+                Defaults to None.
+            logger (logging.Logger | str, optional): Logger used for printing
+                related information during evaluation. Defaults to None.
+        Returns:
+            dict: evaluation results
+        """
+        if metric_options is None:
+            metric_options = {'topk': (1, 5)}
+        if isinstance(metric, str):
+            metrics = [metric]
+        else:
+            metrics = metric
+        allowed_metrics = [
+            'accuracy', 'precision', 'recall', 'f1_score', 'support'
+        ]
+        eval_results = {}
+        # results = np.vstack(results)
+        # gt_labels = self.get_gt_labels()
+        num_imgs = len(results)
+        assert len(gt_labels) == num_imgs, 'dataset testing results should ' \
+                                           'be of the same length as gt_labels.'
+
+        invalid_metrics = set(metrics) - set(allowed_metrics)
+        if len(invalid_metrics) != 0:
+            raise ValueError(f'metric {invalid_metrics} is not supported.')
+
+        topk = metric_options.get('topk', (1, 5))
+        thrs = metric_options.get('thrs')
+        average_mode = metric_options.get('average_mode', 'macro')
+
+        if 'accuracy' in metrics:
+            if thrs is not None:
+                acc = accuracy(results, gt_labels, topk=topk, thrs=thrs)
+            else:
+                acc = accuracy(results, gt_labels, topk=topk)
+            if isinstance(topk, tuple):
+                eval_results_ = {
+                    f'accuracy_top-{k}': a
+                    for k, a in zip(topk, acc)
+                }
+            else:
+                eval_results_ = {'accuracy': acc}
+            if isinstance(thrs, tuple):
+                for key, values in eval_results_.items():
+                    eval_results.update({
+                        f'{key}_thr_{thr:.2f}': value.item()
+                        for thr, value in zip(thrs, values)
+                    })
+            else:
+                eval_results.update(
+                    {k: v.item()
+                     for k, v in eval_results_.items()})
+
+        if 'support' in metrics:
+            support_value = support(
+                results, gt_labels, average_mode=average_mode)
+            eval_results['support'] = support_value
+
+        precision_recall_f1_keys = ['precision', 'recall', 'f1_score']
+        if len(set(metrics) & set(precision_recall_f1_keys)) != 0:
+            if thrs is not None:
+                precision_recall_f1_values = precision_recall_f1(
+                    results, gt_labels, average_mode=average_mode, thrs=thrs)
+            else:
+                precision_recall_f1_values = precision_recall_f1(
+                    results, gt_labels, average_mode=average_mode)
+            for key, values in zip(precision_recall_f1_keys,
+                                   precision_recall_f1_values):
+                if key in metrics:
+                    if isinstance(thrs, tuple):
+                        eval_results.update({
+                            f'{key}_thr_{thr:.2f}': value
+                            for thr, value in zip(thrs, values)
+                        })
+                    else:
+                        eval_results[key] = values
+
+        return eval_results
+
+    def evaluate_bce(self,
+                     results,
+                     gt_labels,
+                     metric='mAP',
+                     metric_options=None,
+                     logger=None,
+                     **deprecated_kwargs):
+        """Evaluate the dataset.
+
+        Args:
+            results (list): Testing results of the dataset.
+            metric (str | list[str]): Metrics to be evaluated.
+                Default value is 'mAP'. Options are 'mAP', 'CP', 'CR', 'CF1',
+                'OP', 'OR' and 'OF1'.
+            metric_options (dict, optional): Options for calculating metrics.
+                Allowed keys are 'k' and 'thr'. Defaults to None
+            logger (logging.Logger | str, optional): Logger used for printing
+                related information during evaluation. Defaults to None.
+            deprecated_kwargs (dict): Used for containing deprecated arguments.
+
+        Returns:
+            dict: evaluation results
+        """
+        if metric_options is None:
+            metric_options = {'thr': 0.5}
+
+        if deprecated_kwargs != {}:
+            warnings.warn('Option arguments for metrics has been changed to '
+                          '`metric_options`.')
+            metric_options = {**deprecated_kwargs}
+
+        if isinstance(metric, str):
+            metrics = [metric]
+        else:
+            metrics = metric
+        allowed_metrics = ['mAP', 'CP', 'CR', 'CF1', 'OP', 'OR', 'OF1']
+        eval_results = {}
+        # results = np.vstack(results)
+        # gt_labels = self.get_gt_labels()
+        num_imgs = len(results)
+        assert len(gt_labels) == num_imgs, 'dataset testing results should ' \
+                                           'be of the same length as gt_labels.'
+
+        invalid_metrics = set(metrics) - set(allowed_metrics)
+        if len(invalid_metrics) != 0:
+            raise ValueError(f'metric {invalid_metrics} is not supported.')
+
+        if 'mAP' in metrics:
+            mAP_value = mAP(results, gt_labels)
+            eval_results['mAP'] = mAP_value
+        if len(set(metrics) - {'mAP'}) != 0:
+            performance_keys = ['CP', 'CR', 'CF1', 'OP', 'OR', 'OF1']
+            performance_values = average_performance(results, gt_labels,
+                                                     **metric_options)
+            for k, v in zip(performance_keys, performance_values):
+                if k in metrics:
+                    eval_results[k] = v
+
+        return eval_results
+
+    def evaluate(self,
+                 results,
+                 metric='accuracy',
+                 metric_options=None,
+                 logger=None):
+        res_end_idx = 0
+        label_end_idx = 0
+        gt_labels = self.get_gt_labels()
+        results = np.vstack(results)
+        final_eval_results = OrderedDict()
+        for idx, file_type in enumerate(self.file_type):
+            res_start_idx = 0 if idx == 0 else res_end_idx
+            res_end_idx = res_end_idx + file_type['max_len']
+            label_start_idx = 0 if idx == 0 else label_end_idx
+            label_end_idx = label_end_idx + 1 if file_type['type'] == 'ce' \
+                else label_end_idx + file_type['max_len']
+            if file_type['type'] == 'ce':
+                eval_results = self.evaluate_ce(
+                    results[..., res_start_idx: res_end_idx],
+                    gt_labels[..., label_start_idx: label_end_idx],
+                    metric='accuracy',
+                    metric_options=None,
+                )
+            elif file_type['type'] == 'bce':
+                eval_results = self.evaluate_ce(
+                    results[..., res_start_idx: res_end_idx],
+                    gt_labels[..., label_start_idx: label_end_idx],
+                    metric=['mAP', 'CP', 'CR', 'CF1', 'OP', 'OR', 'OF1'],
+                    metric_options=None,
+                )
+            else:
+                raise TypeError(f"File type only support 'ce' and 'bce' but got {file_type['type']}")
+            for key, value in eval_results.items():
+                new_key = f'{idx}_{key}'
+                final_eval_results[new_key] = value
+
+        return final_eval_results
---
+++ mmcls/models/losses/hierarchical_cross_entropy.py
@@ -0,0 +1,217 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from ..builder import LOSSES
+from .utils import weight_reduce_loss
+
+
+def cross_entropy(pred,
+                  label,
+                  weight=None,
+                  reduction='mean',
+                  avg_factor=None,
+                  class_weight=None):
+    """Calculate the CrossEntropy loss.
+
+    Args:
+        pred (torch.Tensor): The prediction with shape (N, C), C is the number
+            of classes.
+        label (torch.Tensor): The gt label of the prediction.
+        weight (torch.Tensor, optional): Sample-wise loss weight.
+        reduction (str): The method used to reduce the loss.
+        avg_factor (int, optional): Average factor that is used to average
+            the loss. Defaults to None.
+        class_weight (torch.Tensor, optional): The weight for each class with
+            shape (C), C is the number of classes. Default None.
+
+    Returns:
+        torch.Tensor: The calculated loss
+    """
+    # element-wise losses
+    loss = F.cross_entropy(pred, label, weight=class_weight, reduction='none')
+
+    # apply weights and do the reduction
+    if weight is not None:
+        weight = weight.float()
+    loss = weight_reduce_loss(
+        loss, weight=weight, reduction=reduction, avg_factor=avg_factor)
+
+    return loss
+
+
+def soft_cross_entropy(pred,
+                       label,
+                       weight=None,
+                       reduction='mean',
+                       class_weight=None,
+                       avg_factor=None):
+    """Calculate the Soft CrossEntropy loss. The label can be float.
+
+    Args:
+        pred (torch.Tensor): The prediction with shape (N, C), C is the number
+            of classes.
+        label (torch.Tensor): The gt label of the prediction with shape (N, C).
+            When using "mixup", the label can be float.
+        weight (torch.Tensor, optional): Sample-wise loss weight.
+        reduction (str): The method used to reduce the loss.
+        avg_factor (int, optional): Average factor that is used to average
+            the loss. Defaults to None.
+        class_weight (torch.Tensor, optional): The weight for each class with
+            shape (C), C is the number of classes. Default None.
+
+    Returns:
+        torch.Tensor: The calculated loss
+    """
+    # element-wise losses
+    loss = -label * F.log_softmax(pred, dim=-1)
+    if class_weight is not None:
+        loss *= class_weight
+    loss = loss.sum(dim=-1)
+
+    # apply weights and do the reduction
+    if weight is not None:
+        weight = weight.float()
+    loss = weight_reduce_loss(
+        loss, weight=weight, reduction=reduction, avg_factor=avg_factor)
+
+    return loss
+
+
+def binary_cross_entropy(pred,
+                         label,
+                         weight=None,
+                         reduction='mean',
+                         avg_factor=None,
+                         class_weight=None):
+    r"""Calculate the binary CrossEntropy loss with logits.
+
+    Args:
+        pred (torch.Tensor): The prediction with shape (N, \*).
+        label (torch.Tensor): The gt label with shape (N, \*).
+        weight (torch.Tensor, optional): Element-wise weight of loss with shape
+            (N, ). Defaults to None.
+        reduction (str): The method used to reduce the loss.
+            Options are "none", "mean" and "sum". If reduction is 'none' , loss
+            is same shape as pred and label. Defaults to 'mean'.
+        avg_factor (int, optional): Average factor that is used to average
+            the loss. Defaults to None.
+        class_weight (torch.Tensor, optional): The weight for each class with
+            shape (C), C is the number of classes. Default None.
+
+    Returns:
+        torch.Tensor: The calculated loss
+    """
+    assert pred.dim() == label.dim()
+    # Ensure that the size of class_weight is consistent with pred and label to
+    # avoid automatic boracast,
+    if class_weight is not None:
+        N = pred.size()[0]
+        class_weight = class_weight.repeat(N, 1)
+    loss = F.binary_cross_entropy_with_logits(
+        pred, label, weight=class_weight, reduction='none')
+
+    # apply weights and do the reduction
+    if weight is not None:
+        assert weight.dim() == 1
+        weight = weight.float()
+        if pred.dim() > 1:
+            weight = weight.reshape(-1, 1)
+    loss = weight_reduce_loss(
+        loss, weight=weight, reduction=reduction, avg_factor=avg_factor)
+    return loss
+
+
+@LOSSES.register_module()
+class HierarchicalCrossEntropyLoss(nn.Module):
+
+    def __init__(self,
+                 levels=2,
+                 split=(5, 10),
+                 use_sigmoid=(False,),
+                 use_soft=(False,),
+                 reduction='mean',
+                 loss_weight=1.0,
+                 class_weight=None):
+        super(HierarchicalCrossEntropyLoss, self).__init__()
+        self.use_sigmoid = use_sigmoid
+        self.use_soft = use_soft
+        assert len(split) == levels == len(use_soft) == len(use_sigmoid)
+        self.split = split
+        for idx, (soft, usigmoid) in \
+                enumerate(zip(self.use_soft, self.use_sigmoid)):
+            assert not (
+                    soft and usigmoid
+            ), f'index {idx} in use_sigmoid and use_soft could not be set simultaneously'
+
+        self.reduction = reduction
+        self.loss_weight = loss_weight
+        self.class_weight = class_weight
+        self.cls_criterion = list()
+        self.label_split = [0]
+        for idx in range(levels):
+            start = 0 if idx == 0 else self.split[idx - 1]
+            end = self.split(idx)
+            if self.use_sigmoid:
+                self.cls_criterion.append(binary_cross_entropy)
+                self.label_split.append(self.label_split[-1] + end - start)
+            elif self.use_soft:
+                self.cls_criterion.append(soft_cross_entropy)
+                self.label_split.append(self.label_split[-1] + 1)
+            else:
+                self.cls_criterion.append(cross_entropy)
+                self.label_split.append(self.label_split[-1] + 1)
+        self.label_split = self.label_split[1:]
+
+    def _compute_loss(self,
+                      cls_score,
+                      label,
+                      weight,
+                      class_weight,
+                      reduction,
+                      avg_factor,
+                      **kwargs
+                      ):
+        assert max(self.split) == cls_score.size(-1)
+        loss_cls = []
+        for idx, end in enumerate(self.split):
+            start = 0 if idx == 0 else self.split(idx - 1)
+            label_start = 0 if idx == 0 else self.label_split(idx - 1)
+            label_end = self.label_split[idx]
+            single_loss = self.cls_criterion[idx](cls_score[..., start:end],
+                                                  label[..., label_start:label_end],
+                                                  weight,
+                                                  class_weight=class_weight,
+                                                  reduction=reduction,
+                                                  avg_factor=avg_factor,
+                                                  **kwargs
+                                                  )
+            loss_cls.append(single_loss)
+        return torch.mean(torch.tensor(loss_cls).float())
+
+    def forward(self,
+                cls_score,
+                label,
+                weight=None,
+                avg_factor=None,
+                reduction_override=None,
+                **kwargs):
+        assert reduction_override in (None, 'none', 'mean', 'sum')
+        reduction = (
+            reduction_override if reduction_override else self.reduction)
+
+        if self.class_weight is not None:
+            class_weight = cls_score.new_tensor(self.class_weight)
+        else:
+            class_weight = None
+
+        loss_cls = self.loss_weight * self._compute_loss(
+            cls_score,
+            label,
+            weight,
+            class_weight=class_weight,
+            reduction=reduction,
+            avg_factor=avg_factor,
+            **kwargs)
+        return loss_cls
